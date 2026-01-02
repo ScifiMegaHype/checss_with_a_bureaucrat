dc1a1a28aeda5c6a6e56ff8e085409e6362cc722
ScifiMegaHype <132175395+ScifiMegaHype@users.noreply.github.com>
2026-01-02 19:09:51 +0200

    Changed host

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def on_move(data):
 # ---------------------------------------------------
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, debug=True)
